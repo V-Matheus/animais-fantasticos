e584edf7e5f1a7595ddd56afb3f49a2950d3f0b3
Victor Matheus <victormatheus507@gmail.com>
2023-07-12 10:55:42 -0300

    modal refatorado com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,22 +1,45 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector("[data-modal='abrir']");
-  const botaoFechar = document.querySelector("[data-modal='fechar']");
-  const containermodal = document.querySelector("[data-modal='container']");
+export default class Modal {
+  constructor(botaoAbrir, botaoFechar, containermodal) {
+    this.botaoAbrir = document.querySelector(botaoAbrir);
+    this.botaoFechar = document.querySelector(botaoFechar);
+    this.containermodal = document.querySelector(containermodal);
 
-  if (botaoAbrir && botaoFechar && containermodal) {
-    function toggleModal(event) {
-      event.preventDefault();
-      containermodal.classList.toggle("ativo");
+    // Bind this ao callback para
+    // fazer referencia ao objeto
+    // da classe
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
+  }
+
+  // abre ou fecha o modal
+  toggleModal() {
+    this.containermodal.classList.toggle('ativo');
+  }
+
+  // adiciona o evento de toggle ao modal
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  // Fecha o modal ao clica do lado de fora
+  cliqueForaModal(event) {
+    if (event.target === this.containermodal) {
+      this.toggleModal();
     }
+  }
 
-    function clickForamodal(event) {
-      if (event.target === this) {
-        toggleModal(event);
-      }
+  // Adiciona os eventos aos elementos do modal
+  addModalEvent() {
+    this.botaoAbrir.addEventListener('click', this.eventToggleModal);
+    this.botaoFechar.addEventListener('click', this.eventToggleModal);
+    this.containermodal.addEventListener('click', this.cliqueForaModal);
+  }
+
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containermodal) {
+      this.addModalEvent();
     }
-
-    botaoAbrir.addEventListener("click", toggleModal);
-    botaoFechar.addEventListener("click", toggleModal);
-    containermodal.addEventListener("click", clickForamodal);
+    return this;
   }
 }
