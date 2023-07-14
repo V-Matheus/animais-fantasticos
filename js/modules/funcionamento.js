d287836944e163836a4f71bc1036c70e99bf13ba
Victor Matheus <victormatheus507@gmail.com>
2023-07-14 10:25:49 -0300

    correção de erros ESlint

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,25 +1,25 @@
 export default class Funcionamento {
   constructor(funcionamento, activeClass) {
     this.funcionamento = document.querySelector(funcionamento);
-    this.activeClass = activeClass
+    this.activeClass = activeClass;
   }
 
   dadosFuncionamento() {
-    this.diasSemana = this.funcionamento.dataset.semana.split(",").map(Number);
-    this.horarioSemana = this.funcionamento.dataset.horario.split(",").map(Number);
+    this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
+    this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
   }
 
   dadosAgora() {
     this.dataAgora = new Date();
-    this.diaAgora = dataAgora.getDay();
-    this.horarioAgora = dataAgora.getUTCHours() - 3;
+    this.diaAgora = this.dataAgora.getDay();
+    this.horarioAgora = this.dataAgora.getUTCHours() - 3;
   }
 
   estaAberto() {
     const semanaAberta = this.diasSemana.indexOf(this.diaAgora) !== -1;
-    const horarioAberto = (this.horarioAgora >= this.horarioSemana[0] 
+    const horarioAberto = (this.horarioAgora >= this.horarioSemana[0]
       && this.horarioAgora < this.horarioSemana[1]);
-    return semanaAberta && horarioAberto
+    return semanaAberta && horarioAberto;
   }
 
   ativaAberto() {
